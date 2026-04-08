5dbf2e5e91825417a51990dac98fe1eae3f78b53
ROMAN-2007 <romanyetor@gmail.com>
2026-04-08 11:03:35 +0000

    mise a jour

--- test.js
+++ test.js
@@ -1,29 +1,7 @@
-const chatIcon = document.querySelector('aside a[href="#chatBox"]');
-const chatBox = document.getElementById('chatBox');
-const closeBtn = document.getElementById('closeChat');
-const sendBtn = document.getElementById('sendBtn');
-const chatInputField = document.getElementById('chatInputField');
-const chatMessages = document.getElementById('chatMessages');
+const homeSection = document.getElementById("home");
+const homeIcon = document.querySelector("a[href='#home']");
 
-
-chatIcon.addEventListener('click', (e) => {
-  e.preventDefault();
-  chatBox.classList.toggle('hidden');
-});
-
-closeBtn.addEventListener('click', () => {
-  chatBox.classList.add('hidden');
-});
-
-
-sendBtn.addEventListener('click', () => {
-  const message = chatInputField.value.trim();
-  if (message !== "") {
-    const msgDiv = document.createElement('div');
-    msgDiv.classList.add('message', 'user2');
-    msgDiv.textContent = "Moi: " + message;
-    chatMessages.appendChild(msgDiv);
-    chatInputField.value = "";
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  }
+homeIcon.addEventListener("click", function(e) {
+  e.preventDefault(); // éviter le scroll
+  homeSection.classList.toggle("active"); // toggle: affiche ou cache
 });
